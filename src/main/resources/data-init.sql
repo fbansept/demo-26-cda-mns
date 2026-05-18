788cd262fc15121ad3e99fd31c0753615c5090e8
fbansept <bansept.franck@gmail.com>
2026-05-18 14:15:40 +0200

    hash password

--- src/main/resources/data-init.sql
+++ src/main/resources/data-init.sql
@@ -4,11 +4,11 @@ INSERT INTO role ( name ) VALUES
      ('SUPPLIER');
 
 insert into app_user (role_id, created_at, updated_at, pseudo, email, password)
-values  (1, '2021-03-26 15:58:47.513000', '2026-03-26 15:58:47.513103', 'Utilisateur A', 'a@a.com', 'root'),
-        (2, '2026-10-26 15:58:47.513000', '2026-03-26 15:58:47.513103', 'Utilisateur C', 'c@c.com', 'root'),
-        (3, '2023-03-26 15:58:47.513000', '2026-03-26 15:58:47.513103', 'Utilisateur B', 'b@b.com', 'root'),
-        (3, '2023-03-26 15:58:47.513000', '2026-03-26 15:58:47.513103', 'Utilisateur D', 'd@d.com', 'root'),
-        (null, '2023-03-26 15:58:47.513000', '2026-03-26 15:58:47.513103', 'Utilisateur E', 'e@e.com', 'root');
+values  (1, '2021-03-26 15:58:47.513000', '2026-03-26 15:58:47.513103', 'Utilisateur A', 'a@a.com', '$2a$10$6DXPNMGNIyliJdEQZ.gAqeDp91ug776o5KmuoB/geDq95rqd12Zza'),
+        (2, '2026-10-26 15:58:47.513000', '2026-03-26 15:58:47.513103', 'Utilisateur C', 'c@c.com', '$2a$10$6DXPNMGNIyliJdEQZ.gAqeDp91ug776o5KmuoB/geDq95rqd12Zza'),
+        (3, '2023-03-26 15:58:47.513000', '2026-03-26 15:58:47.513103', 'Utilisateur B', 'b@b.com', '$2a$10$6DXPNMGNIyliJdEQZ.gAqeDp91ug776o5KmuoB/geDq95rqd12Zza'),
+        (3, '2023-03-26 15:58:47.513000', '2026-03-26 15:58:47.513103', 'Utilisateur D', 'd@d.com', '$2a$10$6DXPNMGNIyliJdEQZ.gAqeDp91ug776o5KmuoB/geDq95rqd12Zza'),
+        (null, '2023-03-26 15:58:47.513000', '2026-03-26 15:58:47.513103', 'Utilisateur E', 'e@e.com', '$2a$10$6DXPNMGNIyliJdEQZ.gAqeDp91ug776o5KmuoB/geDq95rqd12Zza');
 
 
 INSERT INTO component (serial_number, name, description, loaner_id) VALUES
